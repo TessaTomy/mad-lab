523ba218c30dd03961150eccc1fa995926aa2338
Tessa Tomy <tessatomy757@gmail.com>
2026-07-28 11:42:45 +0530

    Add SMS and web intent handling in MainActivity

--- 05_implicit-intent-map/MainActivity.java
+++ 05_implicit-intent-map/MainActivity.java
@@ -19,28 +19,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnOpenMap = findViewById(R.id.btnOpenMap);
         Button btnSendEmail = findViewById(R.id.btnSendEmail);
+        Button btnSendMessage = findViewById(R.id.btnSendMessage);
+        Button btnOpenPlayStore = findViewById(R.id.btnOpenWeb);
 
         btnOpenMap.setOnClickListener(this);
         btnSendEmail.setOnClickListener(this);
+        btnSendMessage.setOnClickListener(this);
+        btnOpenPlayStore.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.btnOpenMap) {
+        int id = view.getId();
+
+        if (id == R.id.btnOpenMap) {
             Uri mapUri = Uri.parse("geo:0,0?q=MACE");
             Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
             Intent chooser = Intent.createChooser(mapIntent, "Open Map Using:");
             startActivity(chooser);
 
-        } else if (view.getId() == R.id.btnSendEmail) {
+        } else if (id == R.id.btnSendEmail) {
             Uri emailUri = Uri.parse("mailto:example@email.com");
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO, emailUri);
-
             emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hello from Android App");
             emailIntent.putExtra(Intent.EXTRA_TEXT, "This is the body of the email.");
-
             Intent emailChooser = Intent.createChooser(emailIntent, "Send Email Using:");
             startActivity(emailChooser);
-        }
+
+        } else if (id == R.id.btnSendMessage) {
+            Uri smsUri = Uri.parse("smsto:1234567890");
+            Intent smsIntent = new Intent(Intent.ACTION_SENDTO, smsUri);
+            smsIntent.putExtra("sms_body", "Hello! This is an automated text message.");
+            Intent smsChooser = Intent.createChooser(smsIntent, "Send Message Using:");
+            startActivity(smsChooser);
+
+        } else if (id == R.id.btnOpenWeb) {
+        Uri webUri = Uri.parse("https://developer.android.com");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webUri);
+        startActivity(webIntent);
+    }
     }
 }
